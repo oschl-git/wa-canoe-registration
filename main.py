70aa8e5dc57a22ddc3d491b386427f8f3a4bf6bd
Ondřej Schlaichert <schlaichert.o@gmail.com>
2023-10-23 11:51:48 +0200

    Added saving to JSON

--- main.py
+++ main.py
@@ -1,15 +1,52 @@
 import re
+import json
 
 from flask import Flask, render_template, request, abort, redirect
 from registration import Registration
 
 app = Flask(__name__, static_url_path='/static', static_folder='static', template_folder='templates')
-registrations = []
+
+data_file = 'registrations.json'
+
+
+def load_registrations():
+	try:
+		output = []
+		with open(data_file, 'r') as file:
+			loaded_registrations = json.load(file)
+
+		for r in loaded_registrations:
+			output.append(Registration(
+				r['nick'],
+				r['can_swim'],
+				r['friend']
+			))
+
+		return output
+	except FileNotFoundError:
+		return []
+
+
+def save_registration(new_registration):
+	registrations = load_registrations()
+
+	registrations.append(new_registration)
+
+	output = []
+	for r in registrations:
+		output.append({
+			'nick': r.nick,
+			'can_swim': r.can_swim,
+			'friend': r.friend
+		})
+
+	with open(data_file, 'w') as file:
+		json.dump(output, file, indent=4)
 
 
 @app.route('/', methods=['GET'])
 def index():
-	return render_template('index.html', registrations=registrations), 200
+	return render_template('index.html', registrations=load_registrations()), 200
 
 
 @app.route('/registration', methods=['GET'])
@@ -31,7 +68,7 @@ def register():
 	if nickname_exists(request.form['nick']):
 		abort(400, "Přezdívka již existuje.")
 
-	registrations.append(Registration(
+	save_registration(Registration(
 		request.form['nick'],
 		request.form['can_swim'],
 		request.form['friend'],
@@ -46,6 +83,7 @@ def check_nickname(name):
 
 
 def nickname_exists(name):
+	registrations = load_registrations()
 	for r in registrations:
 		if r.nick == name: return True
 	return False
--- registration.py
+++ registration.py
@@ -1,5 +1,19 @@
+import json
+
+
 class Registration:
 	def __init__(self, nick, can_swim, friend):
 		self.nick = nick
 		self.can_swim = can_swim
 		self.friend = friend
+
+	def serialize(self):
+		registration_dict = {
+			"nick": self.nick,
+			"can_swim": self.can_swim,
+			"friend": self.friend
+		}
+
+		json_string = json.dumps(registration_dict)
+
+		return json_string
